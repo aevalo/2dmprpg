ba47532cd5e7e950361069ab4013ffc4fa61090b
hanshoi <antti.ruotsalainen.vuoma@gmail.com>
2013-05-26 22:38:20 +0300

    some changes to client server, but nothing fully working yet. Lot of commented out code

--- go/2dmprpg-go-server/src/2dmprpg/protocol/command.go
+++ go/2dmprpg-go-server/src/2dmprpg/protocol/command.go
@@ -1,101 +1,90 @@
 package protocol
 
 import (
-  "net"
-  "strconv"
-  "bytes"
-  "fmt"
-  "log"
-  "bufio"
+	"net"
+	"strconv"
+	"bytes"
+	"fmt"
+	"log"
+	"bufio"
+	"time"
 )
-
 
 // Package initialization function
 func init() {
-  // Currently empty
+	// Currently empty
 }
 
 type Command struct {
-  Name string
-  Data string
+	Name string
+	Data string
 }
 
 func NewCommand(name string, data string) *Command {
-  if len(name) == 0 {
-    return nil
-  }
-  c := new(Command)
-  c.Name = name
-  c.Data = data
-  return c
+	if len(name) == 0 {
+		return nil
+	}
+	c := new(Command)
+	c.Name = name
+	c.Data = data
+	return c
 }
 
 func (c *Command) String() string {
-  return fmt.Sprintf("%s%4d%s", c.Name, len(c.Data), c.Data)
+	return fmt.Sprintf("%4d%s%s", len(c.Data), c.Name, c.Data)
 }
 
 func (c *Command) Bytes() []byte {
-  buf := bytes.NewBufferString(c.String())
-  return buf.Bytes()
+	buf := bytes.NewBufferString(c.String())
+	return buf.Bytes()
 }
 
 func ReadCommands(conn *net.Conn) []*Command {
-  cmds := make([]*Command, 0)
-  reader := bufio.NewReader(*conn)
-  buf := make([]byte, 4)
-  //var n int = 0
-  var err error = nil
-  for err == nil {
-    _, err = reader.Read(buf)
-    if err == nil {
-      cmd := NewCommand(string(buf), "")
-      _, err = reader.Read(buf)
-      data_len, err := strconv.Atoi(string(bytes.TrimSpace(buf)))
-      if err == nil {
-        data_buf := make([]byte, data_len)
-        _, err = reader.Read(data_buf)
-        cmd.Data = string(data_buf)
-        cmds = append(cmds, cmd)
-      }
-    } else {
-      log.Printf("Error occured while reading: %v\n", err)
-    }
-  }
-  return cmds
-}
-
-func WriteCommandsArray(conn *net.Conn, cmds []*Command) (int, error) {
-  writer := bufio.NewWriter(*conn)
-  var all int = 0
-  for i := range cmds {
-    n, err := writer.WriteString(cmds[i].String())
-    all = all + n
-    if err != nil {
-      _ = writer.Flush()
-      return all, err
-    }
-    err = writer.Flush()
-    if err != nil {
-      return all, err
-    }
-  }
-  return all, nil
+	cmds := make([]*Command, 0)
+	reader := bufio.NewReader(*conn)
+	buf := make([]byte, 4)
+	var err error = nil
+	for err == nil {
+		tim := time.Time.Now()
+		tim.Add(time.Duration(1) * time.Second)
+		conn.SetDeadline(tim)
+		log.Printf("start reading")
+		_, err = reader.Read(buf)
+		if err == nil {
+			log.Printf("Read length %v\n", string(buf))
+			data_len, err := strconv.Atoi(string(bytes.TrimSpace(buf)))
+			_, err = reader.Read(buf)
+			cmd := NewCommand(string(buf), "")
+			log.Printf("Read commnd %v\n", string(buf))
+			if err == nil {
+				data_buf := make([]byte, data_len)
+				_, err = reader.Read(data_buf)
+				log.Printf("Read data %v\n", string(data_buf))
+				cmd.Data = string(data_buf)
+				cmds = append(cmds, cmd)
+			} else {
+				log.Printf("Error occurred while parsing data: %v\n", err)
+			}
+		}
+	}
+	return cmds
 }
 
 func WriteCommands(conn *net.Conn, cmds ...*Command) (int, error) {
-  writer := bufio.NewWriter(*conn)
-  var all int = 0
-  for i := range cmds {
-    n, err := writer.WriteString(cmds[i].String())
-    all = all + n
-    if err != nil {
-      _ = writer.Flush()
-      return all, err
-    }
-    err = writer.Flush()
-    if err != nil {
-      return all, err
-    }
-  }
-  return all, nil
+	writer := bufio.NewWriter(*conn)
+	var all int = 0
+	for i := range cmds {
+		log.Printf("start writing")
+		n, err := writer.WriteString(cmds[i].String())
+		all = all + n
+		if err != nil {
+			_ = writer.Flush()
+			return all, err
+		}
+		err = writer.Flush()
+		if err != nil {
+			return all, err
+		}
+	}
+	return all, nil
 }
--- go/2dmprpg-go-server/src/2dmprpg/server/main.go
+++ go/2dmprpg-go-server/src/2dmprpg/server/main.go
@@ -1,98 +1,104 @@
 package main
 
 import (
-  "log"
-  "net"
-  "sync"
-  "2dmprpg/protocol"
+	"log"
+	"net"
+	//	"sync"
+	"2dmprpg/protocol"
+	"time"
 )
-
+/*
 func handleConnection(conn *net.Conn) {
-  cmds := protocol.ReadCommands(conn)
-  for i := range cmds {
-    log.Printf("Command #%d: Name: %s, Data: %s\n", i, cmds[i].Name, cmds[i].Data)
-  }
-  log.Println("Writing data...")
-  _, err := protocol.WriteCommands(conn, protocol.NewCommand("SESS", "MP"), protocol.NewCommand("LANG", "<>"))
-  if err != nil {
-    log.Println("Failed to send data:", err)
-  }
-
-  //n, err := protocol.WriteCommands(conn,  protocol.NewCommand("VERS", "<>"),
-  //                                        protocol.NewCommand("LANG", "<>"),
-   //                                       protocol.NewCommand("EXT0", "test"),
-   //                                       protocol.NewCommand("PALT", "file://tmp/mypalette.pal"))
-  //if err != nil {
-  //  fmt.Println("Failed to send data:", err)
-  //}
-  //if n != 4 {
-  //  fmt.Println("Not all commands were sent!")
-  //}
-  //cmds = protocol.ReadCommands(conn)
-  //for i := range cmds {
-  //  fmt.Printf("Command #%d: Name: %s, Data: %s\n", i, cmds[i].Name, cmds[i].Data)
-  //}
-}
+	cmds := protocol.ReadCommands(conn)
+	for i := range cmds {
+		log.Printf("Command #%d: Name: %s, Data: %s\n", i, cmds[i].Name, cmds[i].Data)
+	}
+	log.Println("Writing data...")
+	_, err := protocol.WriteCommands(conn, protocol.NewCommand("SESS", "MP"), protocol.NewCommand("LANG", "<>"))
+	if err != nil {
+		log.Println("Failed to send data:", err)
+	}
+*/
+//n, err := protocol.WriteCommands(conn,  protocol.NewCommand("VERS", "<>"),
+//                                        protocol.NewCommand("LANG", "<>"),
+//                                       protocol.NewCommand("EXT0", "test"),
+//                                       protocol.NewCommand("PALT", "file://tmp/mypalette.pal"))
+//if err != nil {
+//  fmt.Println("Failed to send data:", err)
+//}
+//if n != 4 {
+//  fmt.Println("Not all commands were sent!")
+//}
+//cmds = protocol.ReadCommands(conn)
+//for i := range cmds {
+//  fmt.Printf("Command #%d: Name: %s, Data: %s\n", i, cmds[i].Name, cmds[i].Data)
+//}
+//}
 
 func main() {
-  log.Println("Listening 0.0.0.0:8000 for connections...")
-  ln, err := net.Listen("tcp", "0.0.0.0:8000")
-  if err != nil {
-    log.Println("Error occured while listening for connections:", err)
-    return
-  }
-  if ln != nil {
-    log.Println("Waiting for incoming connections...")
-    conn, err := ln.Accept()
-    if err != nil {
-      log.Println("Failed to accept connection:", err)
-      return
-    } else {
-      if conn != nil {
+	log.Println("Listening 0.0.0.0:8000 for connections...")
+	ln, err := net.Listen("tcp", "0.0.0.0:8000")
+	if err != nil {
+		log.Println("Error occured while listening for connections:", err)
+		return
+	}
+	if ln != nil {
+		log.Println("Waiting for incoming connections...")
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("Failed to accept connection:", err)
+			return
+		} else {
+			if conn != nil {
 
-        log.Println("Handling connection...")
-        var wg sync.WaitGroup
-        // Increment the WaitGroup counter
-        //wg.Add(1)
+				log.Println("Handling connection...")
+				//				var wg sync.WaitGroup
+				// Increment the WaitGroup counter
+				//wg.Add(1)
 
-        // Start the coroutine to handle connection
-        //go func(conn *net.Conn) {
-        //  handleConnection(conn)
-          // Decrement the WaitGroup counter
-        //  wg.Done()
-        //}(&conn)
+				// Start the coroutine to handle connection
+				//go func(conn *net.Conn) {
+				//  handleConnection(conn)
+				// Decrement the WaitGroup counter
+				//  wg.Done()
+				//}(&conn)
 
-        log.Println("Reading data...")
-        wg.Add(1)
-        go func(conn *net.Conn) {
-          cmds := protocol.ReadCommands(conn)
-          for i := range cmds {
-            log.Printf("Command #%d: Name: %s, Data: %s\n", i, cmds[i].Name, cmds[i].Data)
-          }
-          wg.Done()
-        }(&conn)
-        wg.Wait()
+				log.Println("Reading data...")
+				//				wg.Add(1)
+				//				go func(conn *net.Conn) {
+				//			ch := make(chan *protocol.Command, 100)
+				cmds := protocol.ReadCommands(&conn)
+				//				cmd := <-ch				
+				//				log.Printf("Command: Name: %s, Data: %s\n", cmd.Name, cmd.Data)
+				for i := range cmds {
+					log.Printf("Command #%d: Name: %s, Data: %s\n", i, cmds[i].Name, cmds[i].Data)
+				}
+				log.Printf("Reading ended")
+				//				wg.Done()
+				//				}(&conn)
+				//				wg.Wait()
 
-        log.Println("Writing data...")
-        wg.Add(1)
-        go func(conn *net.Conn) {
-          _, err := protocol.WriteCommands(conn, protocol.NewCommand("SESS", "MP"), protocol.NewCommand("LANG", "<>"))
-          if err != nil {
-            log.Println("Failed to send data:", err)
-          }
-          wg.Done()
-        }(&conn)
-        wg.Wait()
+				log.Println("Writing data...")
+				//					wg.Add(1)
+				//					go func(conn *net.Conn) {
+				_, err := protocol.WriteCommands(&conn,
+					protocol.NewCommand("SESS", "MP"),
+					protocol.NewCommand("LANG", "<>"))
+				if err != nil {
+					log.Println("Failed to send data:", err)
+				}
+				//						wg.Done()
+				//					}(&conn)
+				//					wg.Wait()
 
+				log.Println("Closing connection...")
+				err = conn.Close()
+				if err != nil {
+					log.Println("Failed to close connection:", err)
+				}
 
-        log.Println("Closing connection...")
-        err = conn.Close()
-        if err != nil {
-          log.Println("Failed to close connection:", err)
-        }
-
-        log.Println("Done!")
-      }
-    }
-  }
+				log.Println("Done!")
+			}
+		}
+	}
 }
