4d2207a8adfa42621042d002e515438daee7a853
hanshoi <antti.ruotsalainen.vuoma@gmail.com>
2013-05-30 22:27:27 +0300

    added theoretically working copy, testing and debugging still left

--- go/2dmprpg-go-server/src/2dmprpg/main.go
+++ go/2dmprpg-go-server/src/2dmprpg/main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"log"
 	"2dmprpg/server"
+	"os/signal"
 )
 
 func main() {
 	log.Printf("Start application")
 
-	log.Printf("Quit application")
+	server.Start("0.0.0.0", "8000")
+
+	// catch SIGTERM
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			// intterrupt came handle it
+			server.Close()
+			log.Printf("Quit application")
+		}
+	}()
+
+	// main loop
+	for {
+	}
+
 }
